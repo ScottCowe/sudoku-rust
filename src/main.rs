98aaac6df0879f56b698a88f64988126e9f8c76c
Scott Cowe <scott.t.cowe@gmail.com>
2024-05-23 22:00:50 +0100

    changed design a bit to have subgrids

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,93 @@
-struct Grid {
-    completed: bool,
-    rows: [[u8; 9]; 9],
+use rand::Rng;
+
+#[derive(Copy, Clone)]
+struct Subgrid {
+    rows: [[usize; 3]; 3],
 }
 
-fn generate_grid() -> Grid {
-    let mut rows: [[u8; 9]; 9] = [[0; 9]; 9];
+#[derive(Copy, Clone)]
+struct Grid {
+    subgrids: [[Subgrid; 3]; 3],
+}
 
-    // for integers n 1-9
-    // create array from 0-8 and shuffle
-    // for each row
-    //  insert n at i-th index
-    //  set 
-    for i in (1..9) {
-        let mut indexes: [usize; 9] = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ];
+impl Subgrid {
+    fn contains(&self, number: usize) -> bool {
+        for i in 0..9 {
+            let row = i / 3;
+            let col = i - row * 3;
 
-        indexes = shuffle_indexes(indexes);
-
-        for j in (0..9) {
-            let mut n: usize = 0;
-            
-            while rows[j][indexes[j]] != 0 {
-                let temp = indexes[j];
-                indexes[j] = indexes[n];
-                indexes[n] = temp;
-                 
-                n += 1; 
+            if self.rows[row][col] == number {
+                return true;
             }
-
-            rows[j][indexes[j]] = i; 
         }
+
+        return false;
+    }
+}
+
+impl Grid {
+    fn empty() -> Self {
+        let emptySubgrid: Subgrid = Subgrid {
+            rows: [[0; 3]; 3],
+        };
+
+        let subgrids: [[Subgrid; 3]; 3] = [[emptySubgrid; 3]; 3]; 
+
+        let grid = Grid { 
+            subgrids,
+        };
+
+        grid 
     }
 
-    let grid = Grid { 
-        completed: true,
-        rows: rows,
-    };
-    
-    grid 
+    fn populate() -> Self {
+        let mut grid = Self::empty();
+
+        for i in 0..9 {
+            for j in 0..9 {
+                let row = j / 3;
+                let col = j - row * 3;
+
+                grid.subgrids[i / 3][i - (i / 3) * 3].rows[row][col] = j + 1;
+            }
+        }
+
+        grid
+    }
+
+    fn get_rows(&self) -> [[usize; 9]; 9] {
+        let mut rows: [[usize; 9]; 9] = [[0; 9]; 9];
+
+        for i in 0..81 {
+            let row = i / 9;
+            let col = i - row * 9;
+
+            let gridRow = i / 27;
+            let gridCol = (i - gridRow * 27) / 9;
+
+            let subgrid = self.subgrids[gridRow][gridCol];
+
+            let subgridRow = row / 3;
+            let subgridCol = row - subgridRow * 3;
+
+            rows[row][col] = subgrid.rows[subgridRow][subgridCol];
+        }
+
+        rows
+    }
 }
 
 fn shuffle_indexes(indexes: [usize; 9]) -> [usize; 9] {
-    indexes
+    let mut shuffled = indexes;
+
+    for i in (0..7) {
+        let j = rand::thread_rng().gen_range(i..9);
+        let temp = shuffled[i];
+        shuffled[i] = shuffled[j];
+        shuffled[j] = temp;
+    }
+
+    shuffled 
 }
 
 fn print_grid(grid: Grid) {
@@ -56,7 +103,14 @@ fn print_grid(grid: Grid) {
                line.push_str("| "); 
             }
 
-            line.push_str(&format!("{} ", grid.rows[row][col]));
+            let num = grid.get_rows()[row][col];
+
+            if num == 0 {
+                line.push_str("  ");
+            }
+            else {
+                line.push_str(&format!("{} ", num));
+            }
         }
 
         println!("{}|", line);
@@ -66,6 +120,8 @@ fn print_grid(grid: Grid) {
 }
 
 fn main() {
-    let grid: Grid = generate_grid();
+    let grid: Grid = Grid::populate();
     print_grid(grid);
+
+    let rows = grid.get_rows();
 }
